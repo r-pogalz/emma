3779f84dcca608d0e151fc2a225c164b7886b4d5
r-pogalz <r.pogalz@campus.tu-berlin.de>
2016-09-15 20:46:11 +0200

    adjust simple benchmark to use lineitem tbl

--- emma-examples/src/main/scala/eu/stratosphere/emma/examples/coga/Benchmark.scala
+++ emma-examples/src/main/scala/eu/stratosphere/emma/examples/coga/Benchmark.scala
@@ -1,5 +1,3 @@
-package eu.stratosphere.emma.examples.coga
-
 import java.util.concurrent.TimeUnit
 
 import scala.collection.parallel._
@@ -9,7 +7,7 @@ object Benchmark {
 
   val usage =
     """
-    Usage: [--warm-up num] [--rounds num] [--num-threads num] filename
+    Usage: [--warm-up num] [--rounds num] [--num-threads num] [--debug true] filename
     """
 
   val defaultWarmup = 10
@@ -19,6 +17,7 @@ object Benchmark {
   val warmupSym = 'warmup
   val roundsSym = 'rounds
   val numThreadsSym = 'numthreads
+  val debugSym = 'debug
   val infileSym = 'infile
 
   val confidenceCriticalValue = 2.325
@@ -28,55 +27,97 @@ object Benchmark {
     val arglist = args.toList
     type OptionMap = Map[Symbol, Any]
 
-    def toOptionMap(map: OptionMap, list: List[String]): OptionMap = {
+    def toOptionMap(map: OptionMap,
+                    list: List[String]): OptionMap = {
       def isSwitch(s: String) = (s(0) == '-')
       list match {
         case Nil => map
         case "--warm-up" :: value :: tail =>
-          toOptionMap(map ++ Map(warmupSym -> value.toInt), tail)
+          toOptionMap(map ++ Map(warmupSym -> value.toInt),
+                      tail)
         case "--rounds" :: value :: tail =>
-          toOptionMap(map ++ Map(roundsSym -> value.toInt), tail)
+          toOptionMap(map ++ Map(roundsSym -> value.toInt),
+                      tail)
         case "--num-threads" :: value :: tail =>
-          toOptionMap(map ++ Map(numThreadsSym -> value.toInt), tail)
+          toOptionMap(map ++ Map(numThreadsSym -> value.toInt),
+                      tail)
+        case "--debug" :: value :: tail =>
+          toOptionMap(map ++ Map(numThreadsSym -> value.toInt),
+                      tail)
         case string :: opt2 :: tail if isSwitch(opt2) =>
-          toOptionMap(map ++ Map('infile -> string), list.tail)
-        case string :: Nil => toOptionMap(map ++ Map(infileSym -> string), list.tail)
+          toOptionMap(map ++ Map('infile -> string),
+                      list.tail)
+        case string :: Nil => toOptionMap(map ++ Map(infileSym -> string),
+                                          list.tail)
         case option :: tail => throw new IllegalArgumentException("Unknown option " + option)
       }
     }
-    val options = toOptionMap(Map(), arglist)
+    val options = toOptionMap(Map(),
+                              arglist)
 
     val bufferedSource = scala.io.Source.fromFile(options(infileSym).asInstanceOf[String])
-    val buffer = mutable.ParArray.newBuilder[Part]
+    val buffer = mutable.ParArray.newBuilder[Lineitem]
+    var cnt = 0
     for (line <- bufferedSource.getLines) {
+      cnt += 1
+      if (cnt % 100000 == 0) println(s"Read $cnt lines")
       val cols = line.split("\\|").map(_.trim)
-      buffer += Part(cols(0), cols(1), cols(2), cols(3), cols(4), cols(5).toInt, cols(6), cols(7).toDouble, cols(8))
+      buffer += Lineitem(cols(0).toInt,
+                         cols(1).toInt,
+                         cols(2).toInt,
+                         cols(3).toInt,
+                         cols(4).toInt,
+                         cols(5).toDouble,
+                         cols(6).toDouble,
+                         cols(7).toDouble,
+                         cols(8),
+                         cols(9),
+                         cols(10),
+                         cols(11),
+                         cols(12),
+                         cols(13),
+                         cols(14),
+                         cols(15))
     }
     bufferedSource.close
     val parts = buffer.result()
     parts.tasksupport = new ForkJoinTaskSupport(
-      new scala.concurrent.forkjoin.ForkJoinPool(options.getOrElse(numThreadsSym, defaultNumThreads).asInstanceOf[Int]))
+                                                 new scala.concurrent.forkjoin.ForkJoinPool(options
+                                                                                            .getOrElse(numThreadsSym,
+                                                                                             defaultNumThreads)
+                                                                                            .asInstanceOf[Int]))
 
-    compute(parts, options.getOrElse(warmupSym, defaultWarmup).asInstanceOf[Int],
-      options.getOrElse(roundsSym, defaultRounds).asInstanceOf[Int])
+    compute(parts,
+            options.getOrElse(warmupSym,
+                              defaultWarmup).asInstanceOf[Int],
+            options.getOrElse(roundsSym,
+                              defaultRounds).asInstanceOf[Int],
+            options.getOrElse(debugSym,
+                              false).asInstanceOf[Boolean])
   }
 
-  private def compute(parts: ParArray[Part], warmupRounds: Int, times: Int) = {
+  private def compute(lineitems: ParArray[Lineitem],
+                      warmupRounds: Int,
+                      times: Int,
+                      debug: Boolean) = {
     val durationsBuffer = Seq.newBuilder[Long]
     for (i <- 1 to (warmupRounds + times)) {
       //actual algorithm to profile
       val t0 = System.nanoTime()
-      val computed = parts.map(part => part.pSize * part.pRetailPrice)
+      val avgDisc = lineitems.map(l => l.extendedPrice * (1 - l.discount)).sum / lineitems.length
       val t1 = System.nanoTime()
 
       val duration = TimeUnit.NANOSECONDS.toMillis(t1 - t0)
+
+      if (debug) println(s"Average Disc is $avgDisc")
+
       //discard warm up rounds from measurement
       if (i <= warmupRounds) {
-        println(s"Execution time(Warm up round $i): $duration ms")
+        println(s"Execution time(Warm up round $i): ${duration}ms")
       } else {
         durationsBuffer += duration
         //      computed.foreach(println)
-        println(s"Execution time(Round ${i - warmupRounds}): $duration ms")
+        println(s"Execution time(Round ${i - warmupRounds}): ${duration}ms")
       }
     }
     val durations = durationsBuffer.result()
@@ -84,21 +125,36 @@ object Benchmark {
     println("===========================SUMMARY==============================")
     val n = durations.size
     val avg = durations.sum / n
-    val variance = durations.map(d => scala.math.pow(d - avg, 2.0)).sum / n
+    val variance = durations.map(d => scala.math.pow(d - avg,
+                                                     2.0)).sum / n
     val deviation = scala.math.sqrt(variance)
     //calculate 99%-confidence-interval
     val confidenceBorder = confidenceCriticalValue * (deviation / scala.math.sqrt(n))
     val lowerBound = avg - confidenceBorder
     val upperBound = avg + confidenceBorder
 
-    println(s"Min execution time: ${durations.min} ms")
-    println(s"Max execution time: ${durations.max} ms")
-    println(s"Average execution time: $avg ms")
-    println(s"Standard deviation: $deviation ms")
-    println(s"99% confidence interval: [$lowerBound, $upperBound]")
+    println(s"Min execution time: ${durations.min}ms")
+    println(s"Max execution time: ${durations.max}ms")
+    println(s"Average execution time: ${avg}ms")
+    println(s"Standard deviation: ${deviation}ms")
+    println(s"99% confidence interval (in ms): [$lowerBound, $upperBound]")
   }
 
-  case class Part(pPartKey: String, pName: String, pMfgr: String, pBrand: String, pType: String, pSize: Int,
-                  pContainer: String, pRetailPrice: Double, pComment: String)
+  case class Lineitem(orderKey: Int,
+                      partKey: Int,
+                      suppKey: Int,
+                      lineNumber: Int,
+                      quantity: Int,
+                      extendedPrice: Double,
+                      discount: Double,
+                      tax: Double,
+                      returnFlag: String,
+                      lineStatus: String,
+                      shipDate: String,
+                      commitDate: String,
+                      receiptDate: String,
+                      shipInstruct: String,
+                      shipMode: String,
+                      comment: String)
 
 }
--- emma-examples/src/main/scala/eu/stratosphere/emma/examples/coga/BenchmarkOptimized.scala
+++ emma-examples/src/main/scala/eu/stratosphere/emma/examples/coga/BenchmarkOptimized.scala
@@ -1,12 +1,10 @@
-package eu.stratosphere.emma.examples.coga
-
 import java.util.concurrent.{CountDownLatch, TimeUnit}
 
 object BenchmarkOptimized {
 
   val usage =
     """
-    Usage: [--warm-up num] [--rounds num] [--num-threads num] filename
+    Usage: [--warm-up num] [--rounds num] [--num-threads num] [--debug true] filename
     """
 
   val defaultWarmup = 10
@@ -16,6 +14,7 @@ object BenchmarkOptimized {
   val warmupSym = 'warmup
   val roundsSym = 'rounds
   val numThreadsSym = 'numthreads
+  val debugSym = 'debug
   val infileSym = 'infile
 
   val confidenceCriticalValue = 2.325
@@ -25,49 +24,85 @@ object BenchmarkOptimized {
     val arglist = args.toList
     type OptionMap = Map[Symbol, Any]
 
-    def toOptionMap(map: OptionMap, list: List[String]): OptionMap = {
+    def toOptionMap(map: OptionMap,
+                    list: List[String]): OptionMap = {
       def isSwitch(s: String) = (s(0) == '-')
       list match {
         case Nil => map
         case "--warm-up" :: value :: tail =>
-          toOptionMap(map ++ Map(warmupSym -> value.toInt), tail)
+          toOptionMap(map ++ Map(warmupSym -> value.toInt),
+                      tail)
         case "--rounds" :: value :: tail =>
-          toOptionMap(map ++ Map(roundsSym -> value.toInt), tail)
+          toOptionMap(map ++ Map(roundsSym -> value.toInt),
+                      tail)
         case "--num-threads" :: value :: tail =>
-          toOptionMap(map ++ Map(numThreadsSym -> value.toInt), tail)
+          toOptionMap(map ++ Map(numThreadsSym -> value.toInt),
+                      tail)
+        case "--debug" :: value :: tail =>
+          toOptionMap(map ++ Map(debugSym -> value.toBoolean),
+                      tail)
         case string :: opt2 :: tail if isSwitch(opt2) =>
-          toOptionMap(map ++ Map(infileSym -> string), list.tail)
-        case string :: Nil => toOptionMap(map ++ Map('infile -> string), list.tail)
+          toOptionMap(map ++ Map(infileSym -> string),
+                      list.tail)
+        case string :: Nil => toOptionMap(map ++ Map('infile -> string),
+                                          list.tail)
         case option :: tail => throw new IllegalArgumentException("Unknown option " + option)
       }
     }
-    val options = toOptionMap(Map(), arglist)
+    val options = toOptionMap(Map(),
+                              arglist)
 
     val bufferedSource = scala.io.Source.fromFile(options(infileSym).asInstanceOf[String])
-    val buffer = collection.mutable.ArrayBuffer.newBuilder[Part]
+    val buffer = collection.mutable.ArrayBuffer.newBuilder[Lineitem]
+    var cnt = 0
     for (line <- bufferedSource.getLines) {
+      cnt += 1
+      if (cnt % 100000 == 0) println(s"Read $cnt lines")
       val cols = line.split("\\|").map(_.trim)
-      buffer += Part(
-        cols(0), cols(1), cols(2), cols(3), cols(4), cols(5).toInt, cols(6), cols(7).toDouble, cols(8))
+      buffer += Lineitem(cols(0).toInt,
+                         cols(1).toInt,
+                         cols(2).toInt,
+                         cols(3).toInt,
+                         cols(4).toInt,
+                         cols(5).toDouble,
+                         cols(6).toDouble,
+                         cols(7).toDouble,
+                         cols(8),
+                         cols(9),
+                         cols(10),
+                         cols(11),
+                         cols(12),
+                         cols(13),
+                         cols(14),
+                         cols(15))
     }
     bufferedSource.close
-    val parts = buffer.result().toArray
+    val lineitems = buffer.result().toArray
 
-    compute(parts, options.getOrElse(warmupSym, defaultWarmup).asInstanceOf[Int],
-      options.getOrElse(roundsSym, defaultRounds).asInstanceOf[Int],
-      options.getOrElse(numThreadsSym, defaultNumThreads).asInstanceOf[Int])
+    compute(lineitems,
+            options.getOrElse(warmupSym,
+                              defaultWarmup).asInstanceOf[Int],
+            options.getOrElse(roundsSym,
+                              defaultRounds).asInstanceOf[Int],
+            options.getOrElse(numThreadsSym,
+                              defaultNumThreads).asInstanceOf[Int],
+            options.getOrElse(debugSym,
+                              false).asInstanceOf[Boolean])
   }
 
-  private def compute(parts: Array[Part],
-                      warmupRounds: Int, times: Int, numThreads: Int) = {
+  private def compute(lineitems: Array[Lineitem],
+                      warmupRounds: Int,
+                      times: Int,
+                      numThreads: Int,
+                      debug: Boolean) = {
     val durationsBuffer = Seq.newBuilder[Long]
 
     //preparation for threads
-    val results = new Array[Double](parts.length)
+    val results = new Array[Double](numThreads)
     val threads = new Array[Thread](numThreads)
-    val minItemsPerThread = parts.length / numThreads
+    val minItemsPerThread = lineitems.length / numThreads
     val maxItemsPerThread = minItemsPerThread + 1;
-    val threadsWithMaxItems = parts.length % numThreads
+    val threadsWithMaxItems = lineitems.length % numThreads
 
     for (currRound <- 1 to (warmupRounds + times)) {
       //create threads (split work evenly)
@@ -76,7 +111,12 @@ object BenchmarkOptimized {
       for (i <- 0 until numThreads) {
         val itemsCount = if (i < threadsWithMaxItems) maxItemsPerThread else minItemsPerThread
         val end = start + itemsCount
-        threads(i) = new Thread(new ComputationThread(parts, results, start, end, countDownLatch))
+        threads(i) = new Thread(new ComputationThread(lineitems,
+                                                      i,
+                                                      results,
+                                                      start,
+                                                      end,
+                                                      countDownLatch))
         start = end
       }
 
@@ -89,16 +129,20 @@ object BenchmarkOptimized {
       }
       //wait until threads finished execution
       countDownLatch.await()
+      val avgDisc = results.sum / lineitems.length
       val t1 = System.nanoTime()
 
       val duration = TimeUnit.NANOSECONDS.toMillis(t1 - t0)
+      
+      if (debug) println(s"Average Disc is $avgDisc")
+      
       //discard warm up rounds from measurement
       if (currRound <= warmupRounds) {
-        println(s"Execution time(Warm up round $currRound): $duration ms")
+        println(s"Execution time(Warm up round $currRound): ${duration}ms")
       } else {
         durationsBuffer += duration
         //      computed.foreach(println)
-        println(s"Execution time(Round ${currRound - warmupRounds}): $duration ms")
+        println(s"Execution time(Round ${currRound - warmupRounds}): ${duration}ms")
       }
       //clear result array
       for (k <- 0 until results.length) results(k) = 0.0
@@ -108,37 +152,57 @@ object BenchmarkOptimized {
     println("===========================SUMMARY==============================")
     val n = durations.size
     val avg = durations.sum / n
-    val variance = durations.map(d => scala.math.pow(d - avg, 2.0)).sum / n
+    val variance = durations.map(d => scala.math.pow(d - avg,
+                                                     2.0)).sum / n
     val deviation = scala.math.sqrt(variance)
     //calculate 99%-confidence-interval
     val confidenceBorder = confidenceCriticalValue * (deviation / scala.math.sqrt(n))
     val lowerBound = avg - confidenceBorder
     val upperBound = avg + confidenceBorder
 
-    println(s"Min execution time: ${durations.min} ms")
-    println(s"Max execution time: ${durations.max} ms")
-    println(s"Average execution time: $avg ms")
-    println(s"Standard deviation: $deviation ms")
-    println(s"99% confidence interval: [$lowerBound, $upperBound]")
+    println(s"Min execution time: ${durations.min}ms")
+    println(s"Max execution time: ${durations.max}ms")
+    println(s"Average execution time: ${avg}ms")
+    println(s"Standard deviation: ${deviation}ms")
+    println(s"99% confidence interval (in ms): [$lowerBound, $upperBound]")
   }
 
-  class ComputationThread(parts: Array[Part],
-                          results: Array[Double], start: Int, end: Int,
+  class ComputationThread(lineitems: Array[Lineitem],
+                          i: Int,
+                          results: Array[Double],
+                          start: Int,
+                          end: Int,
                           countDownLatch: CountDownLatch) extends Runnable {
 
     def run = {
       //computation
       var j = start
+      var sum = 0.0
       while (j < end) {
-        results(j) = parts(j).pSize * parts(j).pRetailPrice
+        sum += lineitems(j).extendedPrice * (1 - lineitems(j).discount)
         j += 1
       }
+      results(i) = sum
       countDownLatch.countDown()
     }
 
   }
 
-  case class Part(pPartKey: String, pName: String, pMfgr: String, pBrand: String, pType: String, pSize: Int,
-                  pContainer: String, pRetailPrice: Double, pComment: String)
+  case class Lineitem(orderKey: Int,
+                      partKey: Int,
+                      suppKey: Int,
+                      lineNumber: Int,
+                      quantity: Int,
+                      extendedPrice: Double,
+                      discount: Double,
+                      tax: Double,
+                      returnFlag: String,
+                      lineStatus: String,
+                      shipDate: String,
+                      commitDate: String,
+                      receiptDate: String,
+                      shipInstruct: String,
+                      shipMode: String,
+                      comment: String)
 
 }
